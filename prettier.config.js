61c9f3ec64e02b8e1eab044cab6e8759f969980e
Likia <likia0430@outlook.com>
2019-08-22 17:59:23 +0800

    Package setup

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: 'es5',
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+  printWidth: 80,
+};
