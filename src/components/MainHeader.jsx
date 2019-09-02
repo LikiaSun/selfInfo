6bfca6dd34bcbbc2a4f620c5dd707cd252571962
Likia <likia0430@outlook.com>
2019-09-02 09:08:49 +0800

    Use map to render

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -2,50 +2,9 @@ import React from 'react';
 import { useSpring, animated } from 'react-spring';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import RustLogo from '@/assets/images/rust-logo.png';
-import JavaLogo from '@/assets/images/java-logo.png';
-import SvelteLogo from '@/assets/images/svelte-logo.png';
-import PythonLogo from '@/assets/images/python-logo.png';
-import CircleCiLogo from '@/assets/images/circle-ci-logo.png';
+import transList from '../sources/mainTranslateList';
 
 const calc = (x, y) => [x - window.innerHeight / 2, y - window.innerHeight / 2];
-const transGroup = [
-  {
-    order: '1r',
-    icon: 'react',
-    translate: (x, y) =>
-      `translate3d(${x / 3.5 + window.innerWidth / 10}px,${y / 3.5 - 200}px,0)`,
-  },
-  {
-    order: '2v',
-    icon: 'vuejs',
-    translate: (x, y) =>
-      `translate3d(${x / 6 - window.innerWidth / 12}px,${y / 6 - 200}px,0)`,
-  },
-  {
-    order: '3a',
-    icon: 'angular',
-    translate: (x, y) =>
-      `translate3d(${x / 8 - window.innerWidth / 5.5}px,${y / 8 - 25}px,0)`,
-  },
-  {
-    order: '4g',
-    icon: 'git',
-    translate: (x, y) =>
-      `translate3d(${x / 5 - window.innerWidth / 3.5}px,${y / 2.5}px,0)`,
-  },
-  {
-    order: '5n',
-    icon: 'node-js',
-    translate: (x, y) =>
-      `translate3d(${x / 3.5 + window.innerWidth / 5}px,${y / 3.5}px,0)`,
-  },
-  {
-    order: '6s',
-    icon: 'sass',
-    translate: (x, y) => `translate3d(${x / 5}px,${y / 5 - 100}px,0)`,
-  },
-];
 
 const MainHeader = () => {
   const [props, set] = useSpring(() => ({
@@ -58,7 +17,7 @@ const MainHeader = () => {
   return (
     <header
       onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
-      {transGroup.map(transItem => (
+      {transList.iconsGroup.map(transItem => (
         <animated.div
           key={transItem.order}
           className="shape"
@@ -66,68 +25,19 @@ const MainHeader = () => {
           <FontAwesomeIcon icon={['fab', transItem.icon]} size="2x" />
         </animated.div>
       ))}
-      <animated.div
-        className="shape"
-        style={{
-          transform: xy.interpolate(
-            (x, y) => `translate3d(${x / 3.5}px,${y / 3.5 + 100}px,0)`
-          ),
-        }}>
-        <img src={CircleCiLogo} alt="circle-ci logo" width="32" height="32" />
-      </animated.div>
-      <animated.div
-        className="shape"
-        style={{
-          transform: xy.interpolate(
-            (x, y) =>
-              `translate3d(${x / 9 - window.innerWidth / 6}px,${y / 4.5 +
-                100}px,0)`
-          ),
-        }}>
-        <img src={RustLogo} alt="rust logo" width="42" height="42" />
-      </animated.div>
-      <animated.div
-        className="shape"
-        style={{
-          transform: xy.interpolate(
-            (x, y) =>
-              `translate3d(${x / 5.5 + window.innerWidth / 8}px,${y / 2.5}px,0)`
-          ),
-        }}>
-        <img src={PythonLogo} alt="python logo" width="32" height="32" />
-      </animated.div>
-      <animated.div
-        className="shape"
-        style={{
-          transform: xy.interpolate(
-            (x, y) =>
-              `translate3d(${x / 6 - window.innerWidth / 4.5}px,${y / 6 -
-                150}px,0)`
-          ),
-        }}>
-        <img src={SvelteLogo} alt="svelte logo" width="27" height="32" />
-      </animated.div>
-      <animated.div
-        className="shape"
-        style={{
-          transform: xy.interpolate(
-            (x, y) =>
-              `translate3d(${x / 5.5 + window.innerWidth / 8}px,${y / 2.5}px,0)`
-          ),
-        }}>
-        <img src={PythonLogo} alt="python logo" width="32" height="32" />
-      </animated.div>
-      <animated.div
-        className="shape"
-        style={{
-          transform: xy.interpolate(
-            (x, y) =>
-              `translate3d(${x / 2 - window.innerWidth / 8}px,${y / 2 +
-                150}px,0)`
-          ),
-        }}>
-        <img src={JavaLogo} alt="java logo" width="24" height="32" />
-      </animated.div>
+      {transList.picGroup.map(transItem => (
+        <animated.div
+          key={transItem.order}
+          className="shape"
+          style={{ transform: xy.interpolate(transItem.translate) }}>
+          <img
+            src={transItem.source}
+            alt={transItem.alt}
+            width={transItem.width}
+            height={transItem.height}
+          />
+        </animated.div>
+      ))}
       <div className="intro-container">
         <h1 className="intro-title">Likia</h1>
         <span className="intro-subtitle">Full Stack develop</span>
