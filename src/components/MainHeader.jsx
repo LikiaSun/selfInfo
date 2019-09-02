7e2b931ca02069681f60520351f54f451ce30229
Likia <likia0430@outlook.com>
2019-09-02 08:47:01 +0800

    Add some new skill icon

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -3,6 +3,9 @@ import { useSpring, animated } from 'react-spring';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import RustLogo from '@/assets/images/rust-logo.png';
+import JavaLogo from '@/assets/images/java-logo.png';
+import SvelteLogo from '@/assets/images/svelte-logo.png';
+import PythonLogo from '@/assets/images/python-logo.png';
 import CircleCiLogo from '@/assets/images/circle-ci-logo.png';
 
 const calc = (x, y) => [x - window.innerHeight / 2, y - window.innerHeight / 2];
@@ -81,11 +84,53 @@ const MainHeader = () => {
                 100}px,0)`
           ),
         }}>
-        <img src={RustLogo} alt="circle-ci logo" width="42" height="42" />
+        <img src={RustLogo} alt="rust logo" width="42" height="42" />
+      </animated.div>
+      <animated.div
+        className="shape"
+        style={{
+          transform: xy.interpolate(
+            (x, y) =>
+              `translate3d(${x / 5.5 + window.innerWidth / 8}px,${y / 2.5}px,0)`
+          ),
+        }}>
+        <img src={PythonLogo} alt="python logo" width="32" height="32" />
+      </animated.div>
+      <animated.div
+        className="shape"
+        style={{
+          transform: xy.interpolate(
+            (x, y) =>
+              `translate3d(${x / 6 - window.innerWidth / 4.5}px,${y / 6 -
+                150}px,0)`
+          ),
+        }}>
+        <img src={SvelteLogo} alt="svelte logo" width="27" height="32" />
+      </animated.div>
+      <animated.div
+        className="shape"
+        style={{
+          transform: xy.interpolate(
+            (x, y) =>
+              `translate3d(${x / 5.5 + window.innerWidth / 8}px,${y / 2.5}px,0)`
+          ),
+        }}>
+        <img src={PythonLogo} alt="python logo" width="32" height="32" />
+      </animated.div>
+      <animated.div
+        className="shape"
+        style={{
+          transform: xy.interpolate(
+            (x, y) =>
+              `translate3d(${x / 2 - window.innerWidth / 8}px,${y / 2 +
+                150}px,0)`
+          ),
+        }}>
+        <img src={JavaLogo} alt="java logo" width="24" height="32" />
       </animated.div>
       <div className="intro-container">
-        <h1 className="text-white">Likia</h1>
-        <span>Full Stack develop</span>
+        <h1 className="intro-title">Likia</h1>
+        <span className="intro-subtitle">Full Stack develop</span>
         <div className="social-container">
           <a href="https://github.com/LikiaSun" className="social-item">
             <FontAwesomeIcon icon={['fab', 'github-square']} size="2x" />
